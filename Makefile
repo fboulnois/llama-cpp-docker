9858ffd3595d1bb4029bd3f0ec90bf072410ca2a
fboulnois <fboulnois@users.noreply.github.com>
2024-01-03 22:08:03 -0500

    refactor: move download to docker-entrypoint.sh

--- Makefile
+++ Makefile
@@ -11,19 +11,13 @@ else
 endif
 
 llama-2-13b:
-	cd models \
-		&& curl -LO https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q5_K_M.gguf \
-		&& echo "ef36e090240040f97325758c1ad8e23f3801466a8eece3a9eac2d22d942f548a  llama-2-13b-chat.Q5_K_M.gguf" | sha256sum -c -
+	cd models && ../docker-entrypoint.sh $@
 
 mistral-7b:
-	cd models \
-		&& curl -LO https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q5_K_M.gguf \
-		&& echo "b85cdd596ddd76f3194047b9108a73c74d77ba04bef49255a50fc0cfbda83d32  mistral-7b-instruct-v0.2.Q5_K_M.gguf" | sha256sum -c -
+	cd models && ../docker-entrypoint.sh $@
 
 solar-10b:
-	cd models \
-		&& curl -LO https://huggingface.co/TheBloke/SOLAR-10.7B-Instruct-v1.0-GGUF/resolve/main/solar-10.7b-instruct-v1.0.Q5_K_M.gguf \
-		&& echo "4ade240f5dcc253272158f3659a56f5b1da8405510707476d23a7df943aa35f7  solar-10.7b-instruct-v1.0.Q5_K_M.gguf" | sha256sum -c -
+	cd models && ../docker-entrypoint.sh $@
 
 up:
 ifdef HAS_NVIDIA_GPU
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,27 @@
 
 set -eu
 
+# download model if specified as argument and exit
+if [ "$#" -eq 1 ]; then
+    if [ "$1" = "llama-2-13b" ]; then
+        MODEL_URL="https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q5_K_M.gguf"
+        MODEL_SHA256="ef36e090240040f97325758c1ad8e23f3801466a8eece3a9eac2d22d942f548a"
+    elif [ "$1" = "mistral-7b" ]; then
+        MODEL_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q5_K_M.gguf"
+        MODEL_SHA256="b85cdd596ddd76f3194047b9108a73c74d77ba04bef49255a50fc0cfbda83d32"
+    elif [ "$1" = "solar-10b" ]; then
+        MODEL_URL="https://huggingface.co/TheBloke/SOLAR-10.7B-Instruct-v1.0-GGUF/resolve/main/solar-10.7b-instruct-v1.0.Q5_K_M.gguf"
+        MODEL_SHA256="4ade240f5dcc253272158f3659a56f5b1da8405510707476d23a7df943aa35f7"
+    else
+        echo "$0 [llama-2-13b|mistral-7b|solar-10b]"
+        exit 1
+    fi
+    MODEL_NAME=$(basename "$MODEL_URL")
+    curl -LO "$MODEL_URL"
+    echo "$MODEL_SHA256  $MODEL_NAME" | sha256sum -c -
+    exit 0
+fi
+
 # set default environment variables if not set
 if [ -z ${LLAMA_HOST+x} ]; then
     export LLAMA_HOST="0.0.0.0"
