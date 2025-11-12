c608b67ab3d605686907a09175844ecf6d2f7122
fboulnois <fboulnois@users.noreply.github.com>
2025-11-11 20:23:23 -0500

    feat: add qwen3 and magistral models

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,6 +9,7 @@ https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF
 https://huggingface.co/bartowski/Mistral-7B-Instruct-v0.3-GGUF
 https://huggingface.co/bartowski/Mistral-Small-Instruct-2409-GGUF
 https://huggingface.co/bartowski/mistralai_Mistral-Small-3.2-24B-Instruct-2506-GGUF
+https://huggingface.co/bartowski/mistralai_Magistral-Small-2509-GGUF
 https://huggingface.co/bartowski/Ministral-8B-Instruct-2410-GGUF
 https://huggingface.co/bartowski/Phi-3-mini-4k-instruct-GGUF
 https://huggingface.co/bartowski/Phi-3-medium-4k-instruct-GGUF
@@ -21,7 +22,12 @@ https://huggingface.co/bartowski/google_gemma-3-27b-it-GGUF
 https://huggingface.co/bartowski/Qwen2.5-7B-Instruct-GGUF
 https://huggingface.co/bartowski/Qwen2.5-14B-Instruct-GGUF
 https://huggingface.co/bartowski/Qwen2.5-32B-Instruct-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-4B-Instruct-2507-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-30B-A3B-Thinking-2507-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-VL-8B-Instruct-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-VL-30B-A3B-Thinking-GGUF
 https://huggingface.co/bartowski/deepseek-ai_DeepSeek-R1-0528-Qwen3-8B-GGUF
+https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-7B-GGUF
 https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-14B-GGUF
 https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-32B-GGUF
 EOF
