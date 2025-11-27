430e341966697c5d25047cef6ee951c99fab88bc
fboulnois <fboulnois@users.noreply.github.com>
2025-11-27 18:20:53 -0500

    refactor: refresh model list

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,25 +6,18 @@ set -eu
 MODEL_LIST=$(cat << EOF
 https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF
 https://huggingface.co/bartowski/Meta-Llama-3.1-8B-Instruct-GGUF
-https://huggingface.co/bartowski/Mistral-7B-Instruct-v0.3-GGUF
-https://huggingface.co/bartowski/Mistral-Small-Instruct-2409-GGUF
 https://huggingface.co/bartowski/mistralai_Mistral-Small-3.2-24B-Instruct-2506-GGUF
 https://huggingface.co/bartowski/mistralai_Magistral-Small-2509-GGUF
 https://huggingface.co/bartowski/Ministral-8B-Instruct-2410-GGUF
-https://huggingface.co/bartowski/Phi-3-mini-4k-instruct-GGUF
-https://huggingface.co/bartowski/Phi-3-medium-4k-instruct-GGUF
-https://huggingface.co/bartowski/Phi-3.5-mini-instruct-GGUF
 https://huggingface.co/bartowski/microsoft_Phi-4-mini-instruct-GGUF
-https://huggingface.co/bartowski/gemma-2-9b-it-GGUF
-https://huggingface.co/bartowski/gemma-2-27b-it-GGUF
 https://huggingface.co/bartowski/google_gemma-3-12b-it-GGUF
 https://huggingface.co/bartowski/google_gemma-3-27b-it-GGUF
-https://huggingface.co/bartowski/Qwen2.5-7B-Instruct-GGUF
-https://huggingface.co/bartowski/Qwen2.5-14B-Instruct-GGUF
-https://huggingface.co/bartowski/Qwen2.5-32B-Instruct-GGUF
-https://huggingface.co/bartowski/Qwen_Qwen3-4B-Instruct-2507-GGUF
-https://huggingface.co/bartowski/Qwen_Qwen3-30B-A3B-Thinking-2507-GGUF
-https://huggingface.co/bartowski/Qwen_Qwen3-VL-8B-Instruct-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-4B-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-8B-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-14B-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-30B-A3B-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-30B-A3B-Instruct-2507-GGUF
+https://huggingface.co/bartowski/Qwen_Qwen3-VL-30B-A3B-Instruct-GGUF
 https://huggingface.co/bartowski/Qwen_Qwen3-VL-30B-A3B-Thinking-GGUF
 https://huggingface.co/bartowski/deepseek-ai_DeepSeek-R1-0528-Qwen3-8B-GGUF
 https://huggingface.co/bartowski/DeepSeek-R1-Distill-Qwen-7B-GGUF
@@ -37,7 +30,7 @@ usage() {
   MODEL_NAMES=$(echo "$MODEL_LIST" | sed 's|https://huggingface.co/||' | sort)
   cat << EOF
 Set the LLAMA_ARG_HF_REPO environment variable in docker-compose.yml to
-automatically download the model from HuggingFace and use it.
+automatically download and use the model from HuggingFace.
 
 Popular models to download and use:
 
